7d17a22fb83f0e80fdc48a92d4beb30bbc233820
jgsalg <167894430+jgsalg@users.noreply.github.com>
2024-06-02 01:36:19 -0300

    Update models.py

--- finder/models.py
+++ finder/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Disciplina(models.Model):
+    nomedisc = models.CharField(_(""), max_length=7)
+    qtt_provas = models.PositiveSmallIntegerField(_(""))
+    qtt_listas = models.PositiveSmallIntegerField(_(""))
+
+class Exercicios(models.Model):
+    nomearq = models.CharField(_(""), max_length=7)
+    arq = models.BinaryField(_(""))
+    data_submit = models.DateField(_(""), auto_now_add=True)
+    aprovado = models.BooleanField(_(""))
+    data_aprov = models.DateField(_(""))
+
